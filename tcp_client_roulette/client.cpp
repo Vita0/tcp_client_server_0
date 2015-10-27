ebcd327b393dbd974f296bbc0bc530298e0a54f3
Vita0 <bumbum0@ya.ru>
2015-10-27 11:27:31 +0300

    create new tcp projects for game roulette

---
+++ tcp_client_roulette/client.cpp
@@ -0,0 +1,19 @@
+/* 
+ * File:   main.cpp
+ * Author: Vita
+ *
+ * Created on 27 октября 2015 г., 11:00
+ */
+
+#pragma comment(lib,"wsock32.lib")
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return 0;
+}
+
